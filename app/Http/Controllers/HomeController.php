cc4a7bfe31c643b44e4b311e45f8151458ac61e3
iiiaass <asamikannazaki@gmail.com>
2023-03-14 19:46:41 +0900

    memo作成機能　実装

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Memo;  //このコントローラで使うモデルを定義する
+
 
 class HomeController extends Controller
 {
@@ -23,7 +25,31 @@ class HomeController extends Controller
      */
     public function index()
     { 
-        
-        return view('home');
+        $user = \Auth::user();  //ログインしているユーザーの情報を$userに代入
+        $memos = Memo::where('user_id' , $user['id'])->where('status' ,1)->orderby('updated_at','DESC')->get();  //自分が所有しているメモ,かつstatusが１のメモを取得する    //where->取ってくるデータの条件を指定できる   //orderby->並べ方を指定できる(ASC=昇順、DESC=降順)   
+        return view('create',compact('user','memos'));   //conpactを使うことによって$userの情報をviewで使うことが出来る
+    }                                                   
+
+    public function create()
+    {
+        $user = \Auth::user();  //ログインしているユーザーの情報を$userに代入
+        $memos = Memo::where('user_id' , $user['id'])->where('status' ,1)->orderby('updated_at','DESC')->get();  //自分が所有しているメモ,かつstatusが１のメモを取得する    //where->取ってくるデータの条件を指定できる   //orderby->並べ方を指定できる(ASC=昇順、DESC=降順)   
+        return view('create',compact('user','memos'));   //conpactを使うことによって$userの情報をviewで使うことが出来る
+    }
+
+    public function store(Request $request) //Requestを使うと、フォームに入力されたメモの内容、ユーザーidをコントローラで受け取ることが出来る。
+    {
+        $data = $request->all(); //$dataの中に$request allでHTMLから投げられたデータを全て$dataに入れている
+        //dd($data);    //dd->その$dataの中身を分解して画面で確認できる
+        // POSTされたデータをDB（memosテーブル）に挿入
+        // MEMOモデルにDBへ保存する命令を出す
+        $memo_id = Memo::insertGetId([   //insert それぞれデータを定義して、データベースに挿入していっている
+            'content' => $data['content'],
+             'user_id' => $data['user_id'], 
+             'status' => 1
+        ]); 
+
+        // リダイレクト処理->別のページへ遷移すること
+        return redirect()->route('home');
     }
 }
