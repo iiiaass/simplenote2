0092b0d0051fb03bb630eb7ba0c9d5c5ef310161
iiiaass <asamikannazaki@gmail.com>
2023-03-15 13:00:57 +0900

    メモ削除機能　実装

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -2,6 +2,10 @@
 
 @section('content')
     <h4>メモ編集画面</h4>
+    <form method = 'POST' action="/delete/{{$memo['id']}}">
+        @csrf    <!--csrfトークン　フォームに必ず入れるユーザー乗っ取りセキュリティ対策-->
+        <button type = 'submit'>削除</button>
+    </form>
     <form method = 'POST' action = "/update/{{ $memo['id'] }}">
         @csrf    <!--csrfトークン　フォームに必ず入れるユーザー乗っ取りセキュリティ対策-->
         <input type = 'hidden' name='user_id' value = "{{ $user['id'] }}">   <!--hiddenを着けることでユーザーが見えずにデータを渡すことが出来る-->
